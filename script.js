0ad69f179c124376f58ce0822b8d2da71d8acba0
chadmuro <63533886+chadmuro@users.noreply.github.com>
2020-12-30 00:03:37 +0900

    add comments to script.js

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
+// get refereneces to the two color inputs
 const colorOne = document.getElementById('color1');
 const colorTwo = document.getElementById('color2');
+// get reference to the color container
 const colorContainer = document.getElementById('container');
 
-console.log(colorOne.value, colorTwo.value);
-
+// add event listeners to our color inputs to fire function when we change color
 colorOne.addEventListener('change', () => {
-    generateColors(colorOne.value, colorTwo.value);
+	generateColors(colorOne.value, colorTwo.value);
 });
 colorTwo.addEventListener('change', () => {
-    generateColors(colorOne.value, colorTwo.value);
+	generateColors(colorOne.value, colorTwo.value);
 });
 
+// create generateColors function
 const generateColors = (color1, color2) => {
-    // remove child nodes
-    colorContainer.innerHTML = '';
+	// remove any previous child nodes
+	colorContainer.innerHTML = '';
 
-    // get array of colors from chroma.js
-    const colorPalette = chroma.scale([color1, color2]).mode('lch').colors(6);
+	// get array of colors from chroma.js
+	const colorPalette = chroma.scale([color1, color2]).colors(6);
 
-    colorPalette.forEach(color => {
-        const colorItem = document.createElement('div');
-        colorItem.classList.add('container-item');
-        colorItem.style.background = color;
-        colorContainer.appendChild(colorItem);
-    });
-}
+	colorPalette.forEach(color => {
+		// create a div for each color
+		const colorItem = document.createElement('div');
+		// add a class to each div
+		colorItem.classList.add('container-item');
+		// give each div a background color
+		colorItem.style.background = color;
+		// append the div to the container
+		colorContainer.appendChild(colorItem);
+	});
+};
 
+// call the generateColors function initially
 generateColors(colorOne.value, colorTwo.value);
